a133eedf4584d5b98f71c4d8f451944f99c99af6
aishwarya13-r <55542427+aishwarya13-r@users.noreply.github.com>
2026-02-09 15:20:21 -0500

    Refactor HRDataInsert.sql
    Refactor SQL insert statements for clarity and efficiency.

--- HRDataInsert.sql
+++ HRDataInsert.sql
@@ -3,16 +3,13 @@ PRAGMA foreign_keys = ON;
 /* =========================
    regions
    ========================= */
-INSERT INTO regions(region_id, region_name) VALUES
-(1,'Europe'),
-(2,'Americas'),
-(3,'Asia'),
-(4,'Middle East and Africa');
+INSERT INTO regions (region_name) VALUES
+('Europe'),
+('Americas'),
+('Asia'),
+('Middle East and Africa');
 
-/* =========================
-   countries
-   ========================= */
-INSERT INTO countries(country_id, country_name, region_id) VALUES
+INSERT OR IGNORE INTO countries (country_id, country_name, region_id) VALUES
 ('AR','Argentina',2),
 ('AU','Australia',3),
 ('BE','Belgium',1),
@@ -42,86 +39,85 @@ INSERT INTO countries(country_id, country_name, region_id) VALUES
 /* =========================
    locations
    ========================= */
-INSERT INTO locations(location_id, street_address, postal_code, city, state_province, country_id) VALUES
-(1400,'2014 Jabberwocky Rd','26192','Southlake','Texas','US'),
-(1500,'2011 Interiors Blvd','99236','South San Francisco','California','US'),
-(1700,'2004 Charade Rd','98199','Seattle','Washington','US'),
-(1800,'147 Spadina Ave','M5V 2L7','Toronto','Ontario','CA'),
-(2400,'8204 Arthur St',NULL,'London',NULL,'UK'),
-(2500,'Magdalen Centre, The Oxford Science Park','OX9 9ZB','Oxford','Oxford','UK'),
-(2700,'Schwanthalerstr. 7031','80925','Munich','Bavaria','DE');
+INSERT INTO locations (street_address, postal_code, city, state_province, country_id) VALUES
+('2014 Jabberwocky Rd','26192','Southlake','Texas','US'),
+('2011 Interiors Blvd','99236','South San Francisco','California','US'),
+('2004 Charade Rd','98199','Seattle','Washington','US'),
+('147 Spadina Ave','M5V 2L7','Toronto','Ontario','CA'),
+('8204 Arthur St',NULL,'London',NULL,'UK'),
+('Magdalen Centre, The Oxford Science Park','OX9 9ZB','Oxford','Oxford','UK'),
+('Schwanthalerstr. 7031','80925','Munich','Bavaria','DE');
 
 /* =========================
    jobs
    ========================= */
-INSERT INTO jobs(job_id, job_title, min_salary, max_salary) VALUES
-(1,'Public Accountant',4200,9000),
-(2,'Accounting Manager',8200,16000),
-(3,'Administration Assistant',3000,6000),
-(4,'President',20000,40000),
-(5,'Administration Vice President',15000,30000),
-(6,'Accountant',4200,9000),
-(7,'Finance Manager',8200,16000),
-(8,'Human Resources Representative',4000,9000),
-(9,'Programmer',4000,10000),
-(10,'Marketing Manager',9000,15000),
-(11,'Marketing Representative',4000,9000),
-(12,'Public Relations Representative',4500,10500),
-(13,'Purchasing Clerk',2500,5500),
-(14,'Purchasing Manager',8000,15000),
-(15,'Sales Manager',10000,20000),
-(16,'Sales Representative',6000,12000),
-(17,'Shipping Clerk',2500,5500),
-(18,'Stock Clerk',2000,5000),
-(19,'Stock Manager',5500,8500);
+INSERT INTO jobs (job_title, min_salary, max_salary) VALUES
+('Public Accountant',4200,9000),
+('Accounting Manager',8200,16000),
+('Administration Assistant',3000,6000),
+('President',20000,40000),
+('Administration Vice President',15000,30000),
+('Accountant',4200,9000),
+('Finance Manager',8200,16000),
+('Human Resources Representative',4000,9000),
+('Programmer',4000,10000),
+('Marketing Manager',9000,15000),
+('Marketing Representative',4000,9000),
+('Public Relations Representative',4500,10500),
+('Purchasing Clerk',2500,5500),
+('Purchasing Manager',8000,15000),
+('Sales Manager',10000,20000),
+('Sales Representative',6000,12000),
+('Shipping Clerk',2500,5500),
+('Stock Clerk',2000,5000),
+('Stock Manager',5500,8500);
 
 /* =========================
    departments
    ========================= */
-INSERT INTO departments(department_id, department_name, location_id) VALUES
-(1,'Administration',1700),
-(2,'Marketing',1800),
-(3,'Purchasing',1700),
-(4,'Human Resources',2400),
-(5,'Shipping',1500),
-(6,'IT',1400),
-(7,'Public Relations',2700),
-(8,'Sales',2500),
-(9,'Executive',1700),
-(10,'Finance',1700),
-(11,'Accounting',1700);
+INSERT INTO departments (department_name, location_id) VALUES
+('Administration',3),
+('Marketing',4),
+('Purchasing',3),
+('Human Resources',5),
+('Shipping',2),
+('IT',1),
+('Public Relations',7),
+('Sales',6),
+('Executive',3),
+('Finance',3),
+('Accounting',3);
 
 /* =========================
    employees
    ========================= */
-INSERT INTO employees(employee_id, first_name, last_name, email, phone_number, hire_date, job_id, salary, manager_id, department_id) VALUES
-(100,'Steven','King','steven.king@sqltutorial.org','515.123.4567','2000-06-17',4,24000,NULL,9),
-(101,'Neena','Kochhar','neena.kochhar@sqltutorial.org','515.123.4568','2001-09-21',5,17000,100,9),
-(102,'Lex','De Haan','lex.de haan@sqltutorial.org','515.123.4569','1993-01-13',5,17000,100,9),
-(103,'Alexander','Hunold','alexander.hunold@sqltutorial.org','590.423.4567','2012-01-03',9,9000,102,6),
-(104,'Bruce','Ernst','bruce.ernst@sqltutorial.org','590.423.4568','2015-05-21',9,6000,103,6),
-(105,'David','Austin','david.austin@sqltutorial.org','590.423.4569','2012-06-25',9,4800,103,6),
-(106,'Valli','Pataballa','valli.pataballa@sqltutorial.org','590.423.4560','2000-02-05',9,4800,103,6),
-(107,'Diana','Lorentz','diana.lorentz@sqltutorial.org','590.423.5567','2017-02-07',9,4200,103,6),
-(108,'Nancy','Greenberg','nancy.greenberg@sqltutorial.org','515.124.4569','2001-08-17',7,12000,101,10),
-(109,'Daniel','Faviet','daniel.faviet@sqltutorial.org','515.124.4169','2007-08-16',6,9000,108,10),
-(110,'John','Chen','john.chen@sqltutorial.org','515.124.4269','2008-09-28',6,8200,108,10),
-(111,'Ismael','Sciarra','ismael.sciarra@sqltutorial.org','515.124.4369','2009-09-30',6,7700,108,10),
-(112,'Jose Manuel','Urman','jose manuel.urman@sqltutorial.org','2011-03-07',NULL,6,7800,108,10),
-(113,'Luis','Popp','luis.popp@sqltutorial.org','2014-12-07',NULL,6,6900,108,10);
+INSERT INTO employees
+(first_name, last_name, email, phone_number, hire_date, job_id, salary, manager_id, department_id)
+VALUES
+('Steven','King','steven.king@sqltutorial.org','515.123.4567','2000-06-17',4,24000,NULL,9),
+('Neena','Kochhar','neena.kochhar@sqltutorial.org','515.123.4568','2001-09-21',5,17000,1,9),
+('Lex','De Haan','lex.de haan@sqltutorial.org','515.123.4569','1993-01-13',5,17000,1,9),
+('Alexander','Hunold','alexander.hunold@sqltutorial.org','590.423.4567','2012-01-03',9,9000,3,6),
+('Bruce','Ernst','bruce.ernst@sqltutorial.org','590.423.4568','2015-05-21',9,6000,4,6),
+('David','Austin','david.austin@sqltutorial.org','590.423.4569','2012-06-25',9,4800,4,6),
+('Valli','Pataballa','valli.pataballa@sqltutorial.org','590.423.4560','2000-02-05',9,4800,4,6),
+('Diana','Lorentz','diana.lorentz@sqltutorial.org','590.423.5567','2017-02-07',9,4200,4,6),
+('Nancy','Greenberg','nancy.greenberg@sqltutorial.org','515.124.4569','2001-08-17',7,12000,2,10),
+('Daniel','Faviet','daniel.faviet@sqltutorial.org','515.124.4169','2007-08-16',6,9000,9,10),
+('John','Chen','john.chen@sqltutorial.org','515.124.4269','2008-09-28',6,8200,9,10),
+('Ismael','Sciarra','ismael.sciarra@sqltutorial.org','515.124.4369','2009-09-30',6,7700,9,10),
+('Jose Manuel','Urman','jose manuel.urman@sqltutorial.org',NULL,'2011-03-07',6,7800,9,10),
+('Luis','Popp','luis.popp@sqltutorial.org',NULL,'2014-12-07',6,6900,9,10);
 
 /* =========================
    dependents
    ========================= */
-INSERT INTO dependents(dependent_id, first_name, last_name, relationship, employee_id) VALUES
-(1,'Penelope','Gietz','Child',206),
-(2,'Nick','Higgins','Child',205),
-(3,'Ed','Whalen','Child',200),
-(4,'Jennifer','King','Child',100),
-(5,'Johnny','Kochhar','Child',101),
-(6,'Bette','De Haan','Child',102),
-(7,'Grace','Faviet','Child',109),
-(8,'Matthew','Chen','Child',110),
-(9,'Joe','Sciarra','Child',111),
-(10,'Christian','Urman','Child',112),
-(11,'Zero','Popp','Child',113);
+INSERT INTO dependents (first_name, last_name, relationship, employee_id) VALUES
+('Jennifer','King','Child',1),
+('Johnny','Kochhar','Child',2),
+('Bette','De Haan','Child',3),
+('Grace','Faviet','Child',10),
+('Matthew','Chen','Child',11),
+('Joe','Sciarra','Child',12),
+('Christian','Urman','Child',13),
+('Zero','Popp','Child',14);
